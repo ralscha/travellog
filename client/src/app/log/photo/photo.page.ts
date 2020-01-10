6232b9725782185298f5f677d9415ac3b6ce5479
Ralph Schaer <ralphschaer@gmail.com>
2020-01-10 10:42:02 +0100

    More work

--- client/src/app/log/photo/photo.page.ts
+++ client/src/app/log/photo/photo.page.ts
@@ -1,7 +1,6 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {catchError} from 'rxjs/operators';
-import {ActivatedRoute, Router} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-log-photo',
@@ -26,50 +25,17 @@ export class LogPhotoPage implements OnInit {
   }
 
   async onFileChange(event) {
-    const file = event.target.files[0];
-    console.log(file);
+    const files = event.target.files;
     const formData = new FormData();
     formData.append('logId', this.logIdString);
-    formData.append('file', file, file.name);
-    this.httpClient.post<void>(`/be/upload_photo`, formData)
-      .subscribe(ok => {});
-
-
-    const image = new Image();
-    image.onload = () => {
-      const th = this.createThumbnail(image);
-      console.log(th);
+    for (const file of files) {
+      formData.append('file', file, file.name);
     }
-    image.src = URL.createObjectURL(file);
-
+    this.httpClient.post<void>(`/be/upload_photo`, formData)
+      .subscribe(ok => {
+      });
+    
     event.target.value = null;
   }
 
-  private createThumbnail(image: any) {
-    const thumbnailMaxWidth = 100;
-    const thumbnailMaxHeight = 60;
-
-    // create an off-screen canvas
-    const canvas = document.createElement('canvas');
-    const ctx = canvas.getContext('2d');
-
-    // Calculate the size of the thumbnail, to best fit within max/width (cropspadding)
-    const thumbnailScale = (image.width / image.height) > (thumbnailMaxWidth / thumbnailMaxHeight) ?
-      thumbnailMaxWidth / image.width :
-      thumbnailMaxHeight / image.height;
-    const thumbnailWidth = image.width * thumbnailScale;
-    const thumbnailHeight = image.height * thumbnailScale;
-
-    // set its dimension to target size
-    canvas.width = thumbnailWidth;
-    canvas.height = thumbnailHeight;
-
-    // draw source image into the off-screen canvas:
-    ctx.drawImage(image, 0, 0, thumbnailWidth, thumbnailHeight);
-
-    // encode image to data-uri with base64 version of compressed image
-    const thumbnail = new Image();
-    thumbnail.src = canvas.toDataURL('image/jpeg', 70);
-    return thumbnail;
-  }
 }
--- server/src/main/java/ch/rasc/travellog/controller/LogPhotoController.java
+++ server/src/main/java/ch/rasc/travellog/controller/LogPhotoController.java
@@ -4,22 +4,13 @@ import static ch.rasc.travellog.db.tables.Log.LOG;
 import static ch.rasc.travellog.db.tables.LogPhoto.LOG_PHOTO;
 import static ch.rasc.travellog.db.tables.Travel.TRAVEL;
 
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
-
-import javax.imageio.ImageIO;
 
 import org.jooq.DSLContext;
-import org.springframework.http.CacheControl;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,107 +18,133 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.lang.GeoLocation;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.exif.GpsDirectory;
+
 import ch.rasc.travellog.Application;
 import ch.rasc.travellog.config.security.AppUserDetail;
-import net.coobird.thumbnailator.Thumbnails;
-import net.coobird.thumbnailator.Thumbnails.Builder;
 
 @RestController
 @RequestMapping("/be")
 public class LogPhotoController {
 
-  private final DSLContext dsl;
+	private final DSLContext dsl;
 
-  private MessageDigest messageDigest;
+	private MessageDigest messageDigest;
 
-  public LogPhotoController(DSLContext dsl) {
-    this.dsl = dsl;
+	public LogPhotoController(DSLContext dsl) {
+		this.dsl = dsl;
 
-    try {
-      this.messageDigest = MessageDigest.getInstance("MD5");
-    }
-    catch (NoSuchAlgorithmException e) {
-      Application.log.error("get MD5 instance");
-    }
-  }
+		try {
+			this.messageDigest = MessageDigest.getInstance("MD5");
+		}
+		catch (NoSuchAlgorithmException e) {
+			Application.log.error("get MD5 instance");
+		}
+	}
 
-  @PostMapping("delete_photo")
-  public void deletePhoto(@AuthenticationPrincipal AppUserDetail userDetails,
-      @RequestParam("id") long id) {
+	@PostMapping("delete_photo")
+	public void deletePhoto(@AuthenticationPrincipal AppUserDetail userDetails,
+			@RequestParam("id") long id) {
 
-    Set<Long> travelIds = this.dsl.select(TRAVEL.ID).from(TRAVEL)
-        .where(TRAVEL.APP_USER_ID.eq(userDetails.getAppUserId())).fetchSet(TRAVEL.ID);
+		Set<Long> travelIds = this.dsl.select(TRAVEL.ID).from(TRAVEL)
+				.where(TRAVEL.APP_USER_ID.eq(userDetails.getAppUserId()))
+				.fetchSet(TRAVEL.ID);
 
-    Long photoId = this.dsl.select(LOG_PHOTO.ID).from(LOG_PHOTO).join(LOG).onKey()
-        .where(LOG_PHOTO.ID.eq(id).and(LOG.TRAVEL_ID.in(travelIds)))
-        .fetchOne(LOG_PHOTO.ID);
+		Long photoId = this.dsl.select(LOG_PHOTO.ID).from(LOG_PHOTO).join(LOG).onKey()
+				.where(LOG_PHOTO.ID.eq(id).and(LOG.TRAVEL_ID.in(travelIds)))
+				.fetchOne(LOG_PHOTO.ID);
 
-    if (photoId != null) {
-      this.dsl.delete(LOG_PHOTO).where(LOG_PHOTO.ID.eq(photoId)).execute();
-    }
-  }
+		if (photoId != null) {
+			this.dsl.delete(LOG_PHOTO).where(LOG_PHOTO.ID.eq(photoId)).execute();
+		}
+	}
 
-  @PostMapping("upload_photo")
-  public void uploadPhoto(@AuthenticationPrincipal AppUserDetail userDetails,
-      @RequestParam("logId") long logId, @RequestParam("file") MultipartFile file) throws IOException {
+	@PostMapping("upload_photo")
+	public void uploadPhoto(@AuthenticationPrincipal AppUserDetail userDetails,
+			@RequestParam("logId") long logId,
+			@RequestParam("file") List<MultipartFile> files)
+			throws IOException, ImageProcessingException {
 
-    Set<Long> travelIds = this.dsl.select(TRAVEL.ID).from(TRAVEL)
-        .where(TRAVEL.APP_USER_ID.eq(userDetails.getAppUserId())).fetchSet(TRAVEL.ID);
+		Set<Long> travelIds = this.dsl.select(TRAVEL.ID).from(TRAVEL)
+				.where(TRAVEL.APP_USER_ID.eq(userDetails.getAppUserId()))
+				.fetchSet(TRAVEL.ID);
 
-    Long checkLogId = this.dsl.select(LOG.ID).from(LOG)
-        .where(LOG.ID.eq(logId).and(LOG.TRAVEL_ID.in(travelIds))).fetchOne(LOG.ID);
+		Long checkLogId = this.dsl.select(LOG.ID).from(LOG)
+				.where(LOG.ID.eq(logId).and(LOG.TRAVEL_ID.in(travelIds)))
+				.fetchOne(LOG.ID);
 
-    if (checkLogId != null) {
-      System.out.println(file.getName());
-      System.out.println(file.getOriginalFilename());
-      System.out.println(file.getSize());
+		if (checkLogId != null) {
+			System.out.println(files.size());
+			for (MultipartFile file : files) {
+				System.out.println(file.getName());
+				System.out.println(file.getOriginalFilename());
+				System.out.println(file.getSize());
 
-      BufferedImage bi = ImageIO.read(file.getInputStream());
-System.out.println(bi.getHeight());
-      Builder<BufferedImage> thumbnailBuilder = Thumbnails.of(bi);
-     
+				Metadata metadata = ImageMetadataReader
+						.readMetadata(file.getInputStream());
 
-      try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-        thumbnailBuilder.height(100).outputFormat("jpg").keepAspectRatio(true).toOutputStream(baos);
-//        ImageIO.write(resizedBi, "jpg", baos);
-//        baos.flush();
-        byte[] ba = baos.toByteArray();
-        System.out.println("ba");
-        System.out.println(ba.length);
-      }
+				GpsDirectory gpsDirectory = metadata
+						.getFirstDirectoryOfType(GpsDirectory.class);
+				
+				GeoLocation geoLocation = gpsDirectory.getGeoLocation(); 
+			     if (geoLocation != null && !geoLocation.isZero()) { 
+			        System.out.println(geoLocation);
+			     } 
+			     
+					
+			}
+			//
+			// BufferedImage bi = ImageIO.read(file.getInputStream());
+			// System.out.println(bi.getHeight());
+			// Builder<BufferedImage> thumbnailBuilder = Thumbnails.of(bi);
+			//
+			//
+			// try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+			// thumbnailBuilder.height(100).outputFormat("jpg").keepAspectRatio(true).toOutputStream(baos);
+			//// ImageIO.write(resizedBi, "jpg", baos);
+			//// baos.flush();
+			// byte[] ba = baos.toByteArray();
+			// System.out.println("ba");
+			// System.out.println(ba.length);
+			// }
 
-      /*
-       * long id = this.dsl .insertInto(LOG, LOG.CREATED, LOG.LAT, LOG.LNG, LOG.LOCATION,
-       * LOG.REPORT, LOG.UPDATED, LOG.TRAVEL_ID) .values(
-       * LocalDateTime.ofInstant(Instant.ofEpochSecond(clientLog.getCreated()),
-       * ZoneOffset.UTC), clientLog.getLat(), clientLog.getLng(), clientLog.getLocation(),
-       * clientLog.getReport(), now, clientLog.getTravelId())
-       * .returning(LOG.ID).fetchOne().getId();
-       * 
-       * byte[] thedigest = this.messageDigest
-       * .digest(text.getBytes(StandardCharsets.UTF_8)); String key = sourceLanguage + "-"
-       * + targetLanguage + "-" + Base64.getEncoder().encodeToString(thedigest);
-       * 
-       * 
-       * // thumbnail on server
-       * 
-       * InputStream in = new ByteArrayInputStream(ba); BufferedImage bi =
-       * ImageIO.read(in); BufferedImage resizedBi;
-       * 
-       * Builder<BufferedImage> thumbnailBuilder = Thumbnails.of(bi); if (hoehe != null) {
-       * thumbnailBuilder.height(hoehe); } else if (breite != null) {
-       * thumbnailBuilder.width(breite); }
-       * 
-       * resizedBi = thumbnailBuilder.keepAspectRatio(true).asBufferedImage();
-       * 
-       * try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-       * ImageIO.write(resizedBi, "jpg", baos); baos.flush(); ba = baos.toByteArray();
-       * return ResponseEntity.ok().contentLength(ba.length)
-       * .contentType(MediaType.parseMediaType(mediaType))
-       * .cacheControl(CacheControl.maxAge(1, TimeUnit.DAYS)).body(ba); }
-       * 
-       */
-    }
-  }
+			/*
+			 * long id = this.dsl .insertInto(LOG, LOG.CREATED, LOG.LAT, LOG.LNG,
+			 * LOG.LOCATION, LOG.REPORT, LOG.UPDATED, LOG.TRAVEL_ID) .values(
+			 * LocalDateTime.ofInstant(Instant.ofEpochSecond(clientLog.getCreated()),
+			 * ZoneOffset.UTC), clientLog.getLat(), clientLog.getLng(),
+			 * clientLog.getLocation(), clientLog.getReport(), now,
+			 * clientLog.getTravelId()) .returning(LOG.ID).fetchOne().getId();
+			 * 
+			 * byte[] thedigest = this.messageDigest
+			 * .digest(text.getBytes(StandardCharsets.UTF_8)); String key = sourceLanguage
+			 * + "-" + targetLanguage + "-" +
+			 * Base64.getEncoder().encodeToString(thedigest);
+			 * 
+			 * 
+			 * // thumbnail on server
+			 * 
+			 * InputStream in = new ByteArrayInputStream(ba); BufferedImage bi =
+			 * ImageIO.read(in); BufferedImage resizedBi;
+			 * 
+			 * Builder<BufferedImage> thumbnailBuilder = Thumbnails.of(bi); if (hoehe !=
+			 * null) { thumbnailBuilder.height(hoehe); } else if (breite != null) {
+			 * thumbnailBuilder.width(breite); }
+			 * 
+			 * resizedBi = thumbnailBuilder.keepAspectRatio(true).asBufferedImage();
+			 * 
+			 * try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+			 * ImageIO.write(resizedBi, "jpg", baos); baos.flush(); ba =
+			 * baos.toByteArray(); return ResponseEntity.ok().contentLength(ba.length)
+			 * .contentType(MediaType.parseMediaType(mediaType))
+			 * .cacheControl(CacheControl.maxAge(1, TimeUnit.DAYS)).body(ba); }
+			 * 
+			 */
+		}
+	}
 
 }
