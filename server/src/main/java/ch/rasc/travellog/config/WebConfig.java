90ec411b1069cfe8de76240e051152a870f26b6e
Ralph Schaer <ralphschaer@gmail.com>
2020-05-31 06:16:13 +0200

    Upgrade libraries

--- server/src/main/java/ch/rasc/travellog/config/WebConfig.java
+++ server/src/main/java/ch/rasc/travellog/config/WebConfig.java
@@ -2,16 +2,12 @@ package ch.rasc.travellog.config;
 
 import java.time.Duration;
 import java.util.List;
-import java.util.Map;
 
-import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
-import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -22,18 +18,6 @@ import io.github.bucket4j.Refill;
 
 @Configuration
 public class WebConfig {
-
-  // don't send back anything in case of an error
-  @Bean
-  public ErrorAttributes errorAttributes() {
-    return new DefaultErrorAttributes() {
-      @Override
-      public Map<String, Object> getErrorAttributes(WebRequest webRequest,
-          boolean includeStackTrace) {
-        return Map.of();
-      }
-    };
-  }
 
   @Bean
   public WebMvcConfigurer webMvcConfigurer() {
